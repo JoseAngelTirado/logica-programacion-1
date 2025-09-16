3dc28cfd53b5836be5fe5557b483a7b107c34ed0
Jose Angel Tirado <j.angel.tirado20@gmail.com>
2025-09-16 17:59:18 -0600

    implemetacion de segundo metodo

--- diagramaFlujoLogicaProgra.psc
+++ diagramaFlujoLogicaProgra.psc
@@ -5,4 +5,10 @@ Algoritmo sin_titulo
 	Escribir 'Escriba el segundo numero: '
 	Leer segundoNumero
 	Escribir 'Escriba el tercer numero: '
+	Leer tecerNumero
+	Si primerNumero > segundoNumero Entonces
+		Si primerNumero>tercerNumero Entonces
+		
+		FinSi
+	FinSi
 FinAlgoritmo
--- logica-programacion1.js
+++ logica-programacion1.js
@@ -1,16 +1,17 @@
-
+//Declaracion de cada numeros y su solicitud
 let primerNumero = parseInt(prompt("Dame el primer numero: "));
-
-let segundonumero = parseInt(prompt("Dame el segundo numero: "));
-
+let segundoNumero = parseInt(prompt("Dame el segundo numero: "));
 let tercerNumero = parseInt(prompt("Dame el segundo numero: "));
 
-let listaNumeros=[primerNumero, segundonumero, tercerNumero];
-let aux;
+//Aginacion de los numeros a una lista
+let listaNumeros=[primerNumero, segundoNumero, tercerNumero];
+const listaMenorMayor = listaNumeros;
+const listaMayorMenor = listaNumeros;
 
-
-
+//Primer metodo para ordenar la lista, usamos el metodo burbuja
+//
 const menorMayor = (lista)=>{
+    let aux;
     for(i=0;i<lista.length -1;i++){
         for(j=0;j<lista.length -1 -i;j++){
             if(lista[j]>lista[j+1]){
@@ -19,7 +20,7 @@ const menorMayor = (lista)=>{
                 lista[j+1] = aux;
             }
             else if (lista[j]==lista[j+1]) {
-                console.log("Son iguales")
+                console.log(`Son iguales ${lista[j]} y ${lista[j+1]}`)
             }
         }
     }
@@ -28,6 +29,7 @@ const menorMayor = (lista)=>{
 }
 
 const mayorMenor= lista =>{
+    let aux;
     for(i=0;i<lista.length-1;i++){
         for(j=0;j<lista.length-1-i;j++){
             if(lista[j]<lista[j+1]){
@@ -36,14 +38,47 @@ const mayorMenor= lista =>{
                 lista[j+1] = aux;
             }
             else if (lista[j]==lista[j+1]) {
-                console.log("Son iguales")
+                console.log(`Son iguales ${lista[j]} y ${lista[j+1]}`)
             }
         }
     }
     console.log("Numeros ordenados de mayor a menor: ", lista)
     return lista;
 }
+menorMayor(listaMenorMayor);
+mayorMenor(listaMayorMenor);
 
-const listaMenorMayor = menorMayor(listaNumeros);
-const lsitaMayorMenor = mayorMenor(listaNumeros);
+/**
+ * Segundo metodo utlizamos la funcion .sort de JS con if en caso
+ * de que los dos parametros comparados sean iguales
+ */
+// Ordenar de menor a mayor
+const menorMayorSort = [...listaNumeros].sort((a, b) => {
+    const resultados = document.getElementById("resulatados")
+    if (a === b){
+        const li = document.createElement("li");
+        li.textContent = (`Son iguales ${a} y ${b}`)
+        resultados.appendChild(li)
+        
+        return a-b;
+    }
+    return a-b;
+});
+
+const liFinal = document.createElement("li");
+liFinal.textContent = `Ordenados menor a mayor metodo sort: ${menorMayor.join(", ")}`;
+resultados.appendChild(liFinal);
+
+
+
+
+// Ordenar de mayor a menor  
+const mayorAMenorSort = [...listaNumeros].sort((a, b) =>{
+    if(a==b){
+        console.log(`Son iguales ${a} y ${b}`);
+        return b-a;
+    }
+    return b-a;
+});
+console.log("Lista ordenada de menor a mayor por el segundo metodo: ", ordenado)
 
